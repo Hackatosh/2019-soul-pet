c0764d031c426f8392ab748247118811df60c9de
benauwe <edouard.benauw@student.ecp.fr>
2019-11-25 18:49:22 +0100

    Creating react-app

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Welcome to Soul Pet Repository !
 
 ** Launch back without Docker **
+npm install yarn -g
 cd ./back
 yarn install
 yarn dev
--- back/src/routes/health.ts
+++ back/src/routes/health.ts
@@ -3,7 +3,7 @@ import { Request, Response, Router } from 'express';
 const healthRouter = Router();
 
 healthRouter.get('/', (req: Request, res: Response) => {
-  res.send(200);
+  res.sendStatus(200);
 });
 
 export { healthRouter };
