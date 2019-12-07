caf936a8d8618037acc7551dad3ec03067886f13
Antoine Apollis <antoine@apollis.eu>
2019-12-07 15:45:27 +0100

    Export NavBar in barrel file

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { authenticationService } from './services';
-import { PrivateRoute } from './components';
+import { PrivateRoute, NavBar } from './components';
 import { HomePage, LoginPage, RegisterPage } from './pages';
-import { NavBar } from './components/NavBar';
 import { history } from './helpers';
 
 class App extends React.Component<{}, { currentUser: any }> {
