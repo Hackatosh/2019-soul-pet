7e70c8a541ae139fb36d2186640817dafc1dd713
Antoine Apollis <antoine@apollis.eu>
2019-12-07 15:46:30 +0100

    Fix authorization header prefix

--- front/src/helpers/auth-header.ts
+++ front/src/helpers/auth-header.ts
@@ -3,7 +3,7 @@ import { authenticationService } from '../services';
 export function authHeader() {
 	const currentUser = authenticationService.currentUserValue;
 	if (currentUser && currentUser.token) {
-		return { Authorization: `Bearer ${currentUser.token}` };
+		return { Authorization: `JWT ${currentUser.token}` };
 	} else {
 		return { Authorization: '' };
 	}
