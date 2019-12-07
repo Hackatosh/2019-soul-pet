b2c33a53337f4b8b99d52823c73b76ee2a4de849
Antoine Apollis <antoine@apollis.eu>
2019-12-07 01:09:37 +0100

    Add demo home page

--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { authenticationService } from '../services';
 import { User } from '../models';
+import { RouteComponentProps } from 'react-router';
+import parrot from '../resources/animals/parrot.jpg';
+import sheep from '../resources/animals/sheep.jpg';
+import rabbit from '../resources/animals/rabbit.jpg';
+import { Link } from 'react-router-dom';
+import './HomePage.css';
 
-class HomePage extends React.Component<{}, { currentUser: User }> {
-	constructor(props: Readonly<{}>) {
+interface HomePageState {
+	currentUser: User
+}
+
+class HomePage extends React.Component<RouteComponentProps, HomePageState> {
+	constructor(props: RouteComponentProps) {
 		super(props);
 		
 		this.state = {
@@ -14,10 +24,77 @@ class HomePage extends React.Component<{}, { currentUser: User }> {
 	render() {
 		const { currentUser } = this.state;
 		return (
-			<div>
-                <h1>Hi {currentUser.username}!</h1>
-                <p>You’re logged in with React and JWT!</p>
-                <h3>Users from secure api end point:</h3>
+			<div className="container">
+				<div className="row mb-5">
+					<div className="col-sm-6 offset-sm-3">
+						<h1 className="text-center display-4">Bonjour {currentUser.username}&nbsp;!</h1>
+					</div>
+				</div>
+				<div className="row row-cols-2 row-cols-md-3 justify-content-center">
+					<div className="col mb-4">
+						<div className="card">
+							<img src={parrot} className="card-img-top" alt="Parrot" />
+							<div className="card-body">
+								<h5 className="card-title">AraKiri</h5>
+								<p className="card-text">Né le 27 avril 1832 &middot; 22 photos &middot; 3 événements liés</p>
+								<Link to="/animal/5" className="btn btn-primary">Détails</Link>
+							</div>
+						</div>
+					</div>
+					<div className="col mb-4">
+						<div className="card">
+							<img src={sheep} className="card-img-top" alt="Sheep" />
+							<div className="card-body">
+								<h5 className="card-title">Brebiquette</h5>
+								<p className="card-text">Née le 12 décembre 2017 &middot; 13 photos &middot; 32 événements liés</p>
+								<Link to="/animal/4" className="btn btn-primary">Détails</Link>
+							</div>
+						</div>
+					</div>
+					<div className="col mb-4">
+						<div className="card">
+							<img src={rabbit} className="card-img-top" alt="Rabbit" />
+							<div className="card-body">
+								<h5 className="card-title">Marlon Bundo</h5>
+								<p className="card-text">Né le juin 2019 &middot; 127 photos &middot; 1 événement lié</p>
+								<Link to="/animal/7" className="btn btn-primary">Détails</Link>
+							</div>
+						</div>
+					</div>
+					<div className="col mb-4">
+						<div className="card">
+							<img src={parrot} className="card-img-top" alt="Parrot" />
+							<div className="card-body">
+								<h5 className="card-title">AraKiri</h5>
+								<p className="card-text">Né le 27 avril 1832 &middot; 22 photos &middot; 3 événements liés</p>
+								<Link to="/animal/5" className="btn btn-primary">Détails</Link>
+							</div>
+						</div>
+					</div>
+					<div className="col mb-4">
+						<div className="card">
+							<img src={sheep} className="card-img-top" alt="Sheep" />
+							<div className="card-body">
+								<h5 className="card-title">Brebiquette</h5>
+								<p className="card-text">Née le 12 décembre 2017 &middot; 13 photos &middot; 32 événements liés</p>
+								<Link to="/animal/4" className="btn btn-primary">Détails</Link>
+							</div>
+						</div>
+					</div>
+					<div className="col mb-4">
+						<div className="card">
+							<img src={rabbit} className="card-img-top" alt="Rabbit" />
+							<div className="card-body">
+								<h5 className="card-title">Marlon Bundo</h5>
+								<p className="card-text">Né le juin 2019 &middot; 127 photos &middot; 1 événement lié</p>
+								<Link to="/animal/7" className="btn btn-primary">Détails</Link>
+							</div>
+						</div>
+					</div>
+					<div className="col mb-4">
+						<p className="text-center"><Link to="/animal/add" className="btn btn-success">Ajouter un autre animal</Link></p>
+					</div>
+				</div>
             </div>
 		);
 	}
