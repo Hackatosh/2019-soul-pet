2267487fc9822e32e0903b4ed2a3f81f4fc91afb
2017damianib <benoit.damiani@student.ecp.fr>
2019-12-09 15:39:59 +0100

    add places details and request promises

--- back/src/core/placesAPI/getVeterinarian.js
+++ back/src/core/placesAPI/getVeterinarian.js
@@ -1,29 +1,27 @@
 var request = require('request');
+var request_promise = require('request-promise-native');
+var util = require('util');
+var request_ = util.promisify(request);
 var clientId = "OMUJAQFCEKPCCDPHKQW21VUWVJ1YEBGHN0R0RQRQZWJRRNL3";
 var clientSecret = "XQ1BKCP2ARAXAQ4M3AQ0P51CJ5DOVLLVYYINOJWXBXQYEVTZ";
+var veterinarianCategoryId = "4d954af4a243a5684765b473";
 var currentVersion = (new Date()).toISOString().replace(/-/g, '').split('T')[0];
-function getNearestVeterinarian(long, lat, radius) {
-    request({
+function getNearestVeterinarian(pos, radius) {
+    return request_promise({
         url: 'https://api.foursquare.com/v2/venues/search',
         method: 'GET',
         json: true,
         qs: {
             client_id: clientId,
             client_secret: clientSecret,
-            ll: long + ',' + lat,
+            categoryId: veterinarianCategoryId,
+            ll: pos,
             v: currentVersion,
-            radius: radius,
-            query: "veterinaire",
-            limit: 10
-        }
-    }, function (err, res, body) {
-        if (err) {
-            console.error(err);
-        }
-        else {
-            return body.response.venues[0].id;
+            radius: radius
         }
     });
 }
-var result = getNearestVeterinarian("45.676", "4.82115", "5000");
-console.log(result);
+function testPrint(result) {
+    console.log(result.response.venues);
+}
+var promise = getNearestVeterinarian("45.676,4.82115", "10000").then(testPrint);
--- back/src/core/placesAPI/getVeterinarian.ts
+++ back/src/core/placesAPI/getVeterinarian.ts
@@ -1,32 +1,42 @@
-const request = require('request');
 const request_promise = require('request-promise-native');
-const util = require('util');
-const request_ = util.promisify(request);
 
 const clientId = "OMUJAQFCEKPCCDPHKQW21VUWVJ1YEBGHN0R0RQRQZWJRRNL3";
 const clientSecret = "XQ1BKCP2ARAXAQ4M3AQ0P51CJ5DOVLLVYYINOJWXBXQYEVTZ";
+const veterinarianCategoryId = "4d954af4a243a5684765b473";
 let currentVersion = (new Date()).toISOString().replace(/-/g, '').split('T')[0]
 
-function getNearestVeterinarian(long: string, lat: string, radius: string){
-  request({
+function searchVeterinarian(pos: string, radius: string){
+  return request_promise({
     url: 'https://api.foursquare.com/v2/venues/search',
     method: 'GET',
     json: true,
     qs: {
       client_id: clientId,
       client_secret: clientSecret,
-      ll: long+','+lat,
+      categoryId: veterinarianCategoryId,
+      ll: pos,
       v: currentVersion,
-      radius: radius,
-      query: "veterinaire",
-      limit: 10
-    }
-  }, function(err: any, res: any, body: any) {
-    if (err) {
-      console.error(err);
-    } else {
-      return body.response.venues[0].id;
+      radius: radius
     }
   });
 }
+
+function detailsVeterinarian(veterinarianId: string){
+  return request_promise({
+    url: "https://api.foursquare.com/v2/venues/" + veterinarianId,
+    method: 'GET',
+    json: true,
+    qs: {
+      client_id: clientId,
+      client_secret: clientSecret,
+      v: currentVersion
+    }
+  });
+}
+
+function testPrint(result: any) {
+  console.log(result.response.venues);
+}
+
+const promise = searchVeterinarian("45.676,4.82115", "10000").then(testPrint);
 
