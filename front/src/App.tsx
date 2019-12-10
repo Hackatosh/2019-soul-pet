095d43abda9d1a762164ed924a42b80f926d5399
Antoine Apollis <antoine@apollis.eu>
2019-12-10 22:00:56 +0100

    Remove App.state

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
-import { authenticationService } from './services';
 import { PrivateRoute, NavBar } from './components';
 import { HomePage, LoginPage, RegisterPage } from './pages';
 import { history } from './helpers';
 
-class App extends React.Component<{}, { currentUser: any }> {
-    constructor(props: Readonly<{}>) {
-        super(props);
-
-        this.state = {
-            currentUser: null
-        };
-    }
-
-    componentDidMount() {
-        authenticationService.currentUser.subscribe(x => this.setState({ currentUser: x }));
-    }
-
+class App extends React.Component<{}, {}> {
     render() {
         return (
             <Router history={history}>
