554ad28bf4dcb86d524f22bd932313cdf5403b28
Antoine Apollis <antoine@apollis.eu>
2019-12-10 12:43:26 +0100

    Add doc to helpers

--- front/src/helpers/handle-response.ts
+++ front/src/helpers/handle-response.ts
@@ -1,3 +1,8 @@
+/**
+ * Handles an HTTP response depending on its status.
+ * @param response the Response to handle
+ * @returns A Promise containing the parsed JSON of the body
+ */
 export async function handleResponse(response: Response): Promise<any> {
     if (!response.ok)
         return Promise.reject(response.text() || response.statusText);
--- front/src/helpers/http.ts
+++ front/src/helpers/http.ts
@@ -2,7 +2,16 @@ import { authenticationService } from '../services';
 import { handleResponse } from './handle-response';
 import { config } from '../config';
 
+/**
+ * A class designed to handle HTTP calls (`GET`, `POST`, `PUT` and `DELETE`)
+ */
 export class http {
+    /**
+     * Generates headers for an HTTP request.
+     * @param authenticated Indicates whether the request should be authenticated or not.
+     * @param body Indicates whether the request has a body or not.
+     * @returns The generated headers
+     */
     private static headers(authenticated: boolean, body: boolean): Headers {
         const headers = new Headers();
         if (authenticated && authenticationService.currentUserValue && authenticationService.currentUserValue.token) {
@@ -15,6 +24,13 @@ export class http {
         return headers;
     }
 
+    /**
+     * Generates request options for an HTTP request.
+     * @param authenticated Indicates whether the request should be authenticated or not.
+     * @param method Indicates the HTTP method to be used.
+     * @param body Contains the body of the request, if need be.
+     * @returns The generated RequestInit object
+     */
     private static options(authenticated: boolean, method: string, body = ''): RequestInit {
         const hasBody = method !== 'GET' && body !== '';
         const requestOptions: RequestInit = {
@@ -29,6 +45,12 @@ export class http {
         return requestOptions;
     }
 
+    /**
+     * Performs an HTTP request.
+     * @param endpoint The endpoint to send the request to, as a *non-absolute* URL
+     * @param requestOptions A RequestInit object to configure the request
+     * @returns A promise containing an object of type T
+     */
     private static async request<T>(endpoint: string, requestOptions: RequestInit): Promise<T> {
         return fetch(config.apiUrl + endpoint, requestOptions).then(handleResponse).then(data => {
             return data as T;
@@ -37,18 +59,45 @@ export class http {
         });
     }
 
+    /**
+     * Sends a HTTP GET request.
+     * @param endpoint The endpoint to send the request to, as a *non-absolute* URL
+     * @param authenticated Indicates whether the request should be authenticated.
+     * @returns A promise containing an object of type T
+     */
     public static async get<T>(endpoint: string, authenticated = false): Promise<T> {
         return await this.request<T>(endpoint, this.options(authenticated, 'GET'));
     }
 
+    /**
+     * Sends a HTTP POST request.
+     * @param endpoint The endpoint to send the request to, as a *non-absolute* URL
+     * @param authenticated Indicates whether the request should be authenticated.
+     * @param body Contains the body of the request, as a string, if need be.
+     * @returns A promise containing an object of type T
+     */
     public static async post<T>(endpoint: string, authenticated = false, body = ''): Promise<T> {
         return await this.request<T>(endpoint, this.options(authenticated, 'POST', body));
     }
 
+    /**
+     * Sends a HTTP PUT request.
+     * @param endpoint The endpoint to send the request to, as a *non-absolute* URL
+     * @param authenticated Indicates whether the request should be authenticated.
+     * @param body Contains the body of the request, as a string, if need be.
+     * @returns A promise containing an object of type T
+     */
     public static async put<T>(endpoint: string, authenticated = false, body = ''): Promise<T> {
         return await this.request<T>(endpoint, this.options(authenticated, 'PUT', body));
     }
 
+    /**
+     * Sends a HTTP DELETE request.
+     * @param endpoint The endpoint to send the request to, as a *non-absolute* URL
+     * @param authenticated Indicates whether the request should be authenticated.
+     * @param body Contains the body of the request, as a string, if need be.
+     * @returns A promise containing an object of type T
+     */
     public static async delete<T>(endpoint: string, authenticated = false, body = ''): Promise<T> {
         return await this.request<T>(endpoint, this.options(authenticated, 'DELETE', body));
     }
