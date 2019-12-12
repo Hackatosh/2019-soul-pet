90ad9f718635735cd6d38e7dff19faa8b2710846
Antoine Apollis <antoine@apollis.eu>
2019-12-12 13:08:07 +0100

    Improve httpClient

--- front/src/services/authentication.service.ts
+++ front/src/services/authentication.service.ts
@@ -11,7 +11,7 @@ export class AuthenticationService {
      * @returns The user as sent by the API
      */
     static async login(email: string, password: string): Promise<User> {
-        return httpClient.post<User>('/auth/login', false, JSON.stringify({ email, password })).then(user => {
+        return httpClient.post<User>('/auth/login', { email: email, password: password }).then(user => {
             localStorage.setItem('user', JSON.stringify(user));
             history.push('/');
             return user;
@@ -26,7 +26,7 @@ export class AuthenticationService {
      * @returns The user as registered by the API
      */
     static async register(username: string, email: string, password: string): Promise<User> {
-        return httpClient.post<User>('/auth/register', false, JSON.stringify({ username, email, password })).then(null, () => Promise.reject('Création du compte impossible ; veuillez réessayer.'));
+        return httpClient.post<User>('/auth/register', { username: username, email: email, password: password }).then(null, () => Promise.reject('Création du compte impossible ; veuillez réessayer.'));
     }
     
     /**
