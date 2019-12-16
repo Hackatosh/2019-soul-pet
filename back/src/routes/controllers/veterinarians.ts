ea17c40cebb16412fbb0247cc184b8383093feee
2017damianib <benoit.damiani@student.ecp.fr>
2019-12-16 10:53:13 +0100

    add checks

--- back/src/routes/controllers/veterinarians.ts
+++ back/src/routes/controllers/veterinarians.ts
@@ -1,13 +1,21 @@
-import Router from "express";
+import {Response, Router} from "express";
 import {searchVeterinarian} from "../../core/placesAPI/getVeterinarian";
 import {AuthenticatedRequest} from "../../core/authentication/authenticationInterfaces";
+import {check} from "express-validator";
+import {inputValidationMW} from "../middlewares/inputValidation";
 
 const veterinariansRouter = Router();
 
 /** This route is used to search for veterinarians near the user **/
 
-veterinariansRouter.get('/search', async (req: any, res: any) => {
-    const searchPromise = searchVeterinarian(req.query.lat, req.query.long, req.query.radius);
+const getSearchVeterinariansCheck = [
+    check('lat').notEmpty().isNumeric().withMessage("lat must be a number"),
+    check('long').notEmpty().isNumeric().withMessage("lat must be a number"),
+    check('radius').notEmpty().isNumeric().withMessage("lat must be a number")
+]
+
+veterinariansRouter.get('/search', getSearchVeterinariansCheck, inputValidationMW, async (req: AuthenticatedRequest, res: Response) => {
+    const searchPromise = searchVeterinarian(parseFloat(req.query.lat), parseFloat(req.query.long), parseInt(req.query.radius));
     await searchPromise.then((result: any) => {
         res.send(result)
     })
