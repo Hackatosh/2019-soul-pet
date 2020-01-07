4463a81b7e2af1003f9cf7b4ff47ac685c947527
Antoine Apollis <antoine@apollis.eu>
2020-01-07 12:09:56 +0100

    Add tests for prettifiers

--- front/src/helpers/prettifiers.ts
+++ front/src/helpers/prettifiers.ts
@@ -2,12 +2,17 @@
  * Computes the number of full years elapsed since a given date.
  */
 export function ageFromDate(date: Date): number {
-    return Math.floor(((new Date()).getTime() - date.getTime()) / 31536000000);
+    const now = new Date();
+    if (date > now)
+        throw new Error('La date ne peut pas être dans le futur !');
+    return Math.floor((now.getTime() - date.getTime()) / 31536000000);
 }
 
 /**
  * Puts the first letter of a string in uppercase.
  */
 export function titleCase(s: string): string {
+    if (s.length === 0)
+        return s
     return s[0].toUpperCase() + s.substr(1);
 }
