943d9dff83475519e4bb2a2f2550d859e0d5e8af
Raklyon <alexis.hubert@student.ecp.fr>
2020-01-16 09:29:09 +0100

    Added shadow image

--- front/src/models/MarkerIcon.ts
+++ front/src/models/MarkerIcon.ts
@@ -2,31 +2,24 @@ import L from 'leaflet';
 import imageVet from '../resources/icons/001-maps-and-flags-1.png';
 import imagePark from '../resources/icons/002-maps-and-flags-2.png';
 import imageGroom from '../resources/icons/003-maps-and-flags-3.png';
-
+import shadow from '../resources/icons/shadow.png';
 
 const iconPark = L.icon({
   iconUrl: imagePark,
-  iconSize: [26, 40],
-  shadowSize:   [50, 64], // size of the shadow
-  iconAnchor: [22, 94],
-  popupAnchor: [-3, -76],
-  shadowAnchor: [4, 62],  // the same for the shadow
+  shadowUrl: shadow,
+  iconSize: [26, 40]
 });
 
 const iconVet = L.icon({
   iconUrl: imageVet,
-  iconSize: [26, 40],
-  shadowSize:   [50, 64], // size of the shadow
-  iconAnchor: [22, 94],
-  popupAnchor: [-3, -76],
-  shadowAnchor: [4, 62],  // the same for the shadow
+  shadowUrl: shadow,
+  iconSize: [26, 40]
 });
 
 const iconGroom = L.icon({
+  shadowUrl: shadow,
   iconUrl: imageGroom,
-  iconSize: [26, 40],
-  iconAnchor: [22, 94],
-  popupAnchor: [-3, -76],
+  iconSize: [26, 40]
 });
 
 
