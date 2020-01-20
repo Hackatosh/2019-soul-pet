86a4304435d61256fb002d3fcbc9f28efd3bf379
Antoine Apollis <antoine@apollis.eu>
2020-01-20 13:29:35 +0100

    Load animal images in the relevant pages

--- front/src/components/AnimalCard.tsx
+++ front/src/components/AnimalCard.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import parrot from '../resources/animals/parrot.jpg';
 import { Animal } from '../models';
+import { PictureService, AnimalService } from '../services';
+import { SquareImage } from './SquareImage';
 
 export interface AnimalCardProps {
-	animal: Animal
+	animal: Animal;
 }
 
-export class AnimalCard extends React.Component<AnimalCardProps, {}> {
+export interface AnimalCardState {
+    picture: string;
+    pictureCount: number;
+}
+
+export class AnimalCard extends React.Component<AnimalCardProps, AnimalCardState> {
+    constructor(props: AnimalCardProps) {
+        super(props);
+        this.state = { picture: '', pictureCount: 0 };
+    }
+
+    componentDidMount() {
+        if (this.props.animal.id !== undefined)
+            AnimalService.getPictures(this.props.animal.id).then(pictures => {
+                this.setState({ pictureCount: pictures.length });
+                if (pictures.length > 1)
+                    PictureService.getPicture('animals', pictures[pictures.length - 1].filename).then(p => this.setState({ picture: p }))
+            });
+    }
+
     render() {
-		return (
-			<div className="card ">
-				<img src={parrot} className="card-img-top" alt="Parrot" />
+        return (
+			<div className="card">
+                <SquareImage image={this.state.picture}/>
 				<div className="card-body">
 					<h5 className="card-title">{this.props.animal.name}</h5>
-					<p className="card-text">Né le {this.props.animal.birthdate.toLocaleDateString()} &middot; 0 photo &middot; {this.props.animal.events ? this.props.animal.events.length : 0} événement{this.props.animal.events && this.props.animal.events.length > 1 && "s"}</p>
+					<p className="card-text">Né le {this.props.animal.birthdate.toLocaleDateString()} &middot; {this.state.pictureCount} photo{this.state.pictureCount > 1 && 's'} &middot; {this.props.animal.events ? this.props.animal.events.length : 0} événement{this.props.animal.events && this.props.animal.events.length > 1 && "s"}</p>
 					<Link to={"/animal/" + this.props.animal.id} className="btn btn-primary">Détails</Link>
 				</div>
 			</div>
--- front/src/pages/AnimalPage.tsx
+++ front/src/pages/AnimalPage.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { AnimalService } from '../services';
+import { AnimalService, PictureService } from '../services';
 import { RouteComponentProps } from 'react-router';
 import './HomePage.css';
-import { AnimalForm, DeleteConfirmation } from '../components';
-import { Animal } from '../models';
+import { AnimalForm, DeleteConfirmation, SquareImage } from '../components';
+import { Animal, Picture } from '../models';
 import { Card, Button } from 'react-bootstrap';
 import { history, titleCase, ageFromDate } from '../helpers';
-import sheep from '../resources/animals/sheep.jpg';
 
 export interface AnimalPageProps extends RouteComponentProps<{id: string}> {}
 
@@ -14,8 +13,9 @@ export interface AnimalPageState {
     error: string;
     id: number;
     animal: Animal | undefined;
+    pictures: string[];
 	showAnimalForm: boolean;
-	showAnimalDelete: boolean;
+    showAnimalDelete: boolean;
 }
 
 export class AnimalPage extends React.Component<AnimalPageProps, AnimalPageState> {
@@ -24,11 +24,21 @@ export class AnimalPage extends React.Component<AnimalPageProps, AnimalPageState
 		if (this.props.match.params.id === undefined || isNaN(parseInt(this.props.match.params.id)))
 			history.push('/404')
 		else
-            this.state = { error: '', id: parseInt(this.props.match.params.id), animal: undefined, showAnimalForm: false, showAnimalDelete: false };
+            this.state = { error: '', id: parseInt(this.props.match.params.id), animal: undefined, pictures: [], showAnimalForm: false, showAnimalDelete: false };
 	}
 
     componentDidMount() {
-        AnimalService.get(this.state.id).then(a => this.setState({ animal: a })).catch(() => history.push('/404'));   
+        AnimalService.get(this.state.id).then(a => {
+            this.setState({ animal: a });
+            if (a.animalPictures !== undefined && a.animalPictures.length > 0) {
+                let pictures = new Array<string>(a.animalPictures.length).fill('');
+                this.setState({ pictures: pictures });
+                a.animalPictures.forEach((p: Picture, i: number) => PictureService.getPicture('animals', p.filename).then(c => {
+                    pictures[i] = c;
+                    this.setState({ pictures: pictures });
+                }));
+            }
+        }).catch(() => history.push('/404'));   
 	}
 
     private showAnimalForm(state: boolean) {
@@ -46,45 +56,12 @@ export class AnimalPage extends React.Component<AnimalPageProps, AnimalPageState
                 <React.Fragment>
 					<div className="row">
 						<div className="col-10 offset-1 col-md-3">
-							<img className="rounded img-fluid" src={sheep} alt={"Picture of " + this.state.animal.name} />
+                            <SquareImage image={this.state.pictures[this.state.pictures.length - 1]}/>
 						</div>
 						<div className="col-10 offset-1 offset-md-0 col-md-7">
 							{this.state.error !== '' && <div className="alert alert-danger">{this.state.error}</div>}
 							<h1 className="display-3">{this.state.animal.name}</h1>
 							<p className="lead text-muted">{this.state.animal.specie !== undefined ? titleCase(this.state.animal.specie?.name) : ''} né le {this.state.animal.birthdate.toLocaleDateString()} ({ageFromDate(this.state.animal.birthdate)} ans) &middot; <Button variant="primary" onClick={() => this.showAnimalForm(true)}>Éditer</Button> &middot; <Button variant="danger" onClick={() => this.showAnimalDelete(true)}>Supprimer</Button></p>
-							<h2>Services préférés</h2>
-							<div className="row row-cols-1 row-cols-md-3">
-								<div className="col mb-4">
-									<Card>
-										<Card.Body>
-											<Card.Title>Dark Vador</Card.Title>
-											<Card.Subtitle className="mb-2 text-muted">Vétérinaire</Card.Subtitle>
-											<Card.Text>Spécialisé pour nos amis canins.<br/>Fermé le lundi</Card.Text>
-											<Card.Link href="#">Voir la fiche</Card.Link>
-										</Card.Body>
-									</Card>
-								</div>
-								<div className="col mb-4">
-									<Card>
-										<Card.Body>
-											<Card.Title>Kristoff</Card.Title>
-											<Card.Subtitle className="mb-2 text-muted">Vétérinaire</Card.Subtitle>
-											<Card.Text>J’aime les rennes.<br/>Fermé l’hiver</Card.Text>
-											<Card.Link href="#">Voir la fiche</Card.Link>
-										</Card.Body>
-									</Card>
-								</div>
-								<div className="col mb-4">
-									<Card>
-										<Card.Body>
-											<Card.Title>Yennefer</Card.Title>
-											<Card.Subtitle className="mb-2 text-muted">Toiletteuse</Card.Subtitle>
-											<Card.Text>J’ai pas encore regardé mais ça ne saurait tarder.</Card.Text>
-											<Card.Link href="#">Voir la fiche</Card.Link>
-										</Card.Body>
-									</Card>
-								</div>
-							</div>
 							<h2>Événements</h2>
 							<div className="row row-cols-1 row-cols-md-3">
 								<div className="col mb-4">
@@ -117,6 +94,10 @@ export class AnimalPage extends React.Component<AnimalPageProps, AnimalPageState
 										</Card.Body>
 									</Card>
 								</div>
+							</div>
+                            <h2>Galerie</h2>
+							<div className="row row-cols-1 row-cols-md-3">
+                                {this.state.pictures.map((picture: string, index: number) => <div className="col mb-4"><SquareImage image={picture} key={index} /></div>)}
 							</div>
 						</div>
 					</div>
