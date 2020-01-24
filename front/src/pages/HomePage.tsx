6110af79cb49749bc0c17245eb0e364dc226f463
Antoine Apollis <antoine@apollis.eu>
2020-01-24 13:19:36 +0100

    Redirect to animal profile upon addition

--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import { Button } from 'react-bootstrap';
 import './HomePage.css';
 import { AnimalCard, AnimalForm } from '../components';
 import { Animal } from '../models';
+import { history } from '../helpers';
 
 export interface HomePageState {
     error: string;
@@ -46,7 +47,7 @@ export class HomePage extends React.Component<RouteComponentProps, HomePageState
 				<div className="row row-cols-1 row-cols-md-3 justify-content-center">
 					{this.state.animals.map(a => <div className="col mb-4" key={a.id}><AnimalCard animal={a} /></div>)}
 				</div>
-                <AnimalForm show={this.state.showAnimalForm} onHide={() => this.showAnimalForm(false)} onSuccess={(animal: Animal) => this.setState({ animals: [animal].concat(this.state.animals) })} />
+                <AnimalForm show={this.state.showAnimalForm} onHide={() => this.showAnimalForm(false)} onSuccess={(animal: Animal) => history.push(`animal/${animal.id}`) } />
             </div>
 		);
 	}
