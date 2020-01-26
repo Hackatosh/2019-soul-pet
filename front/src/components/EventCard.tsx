70a750beb5eb276a5a85332a72ee45fe86dbb297
Antoine Apollis <antoine@apollis.eu>
2020-01-26 21:19:41 +0100

    Improve event card UI

--- front/src/components/EventCard.tsx
+++ front/src/components/EventCard.tsx
@@ -7,24 +7,26 @@ interface EventCardProps {
     event: PetEvent;
 }
 
-export class EventCard extends Component<EventCardProps, {}> {
-
+export class EventCard extends Component<EventCardProps> {
     render() {
 		const {event} = this.props;
+		const isSameDay = event.beginDate.getTime() === event.endDate.getTime();
+
         return (
             <div className="col mb-4">
                 <div className="card">
                     <img src={peche} className="card-img-top" alt="Peche"/>
                     <div className="card-body">
-                        <h5 className="card-title">{event.name}</h5>
-                        <h6 className="card-title">Organisé par {event.user ? event.user.username : "Inconnu·e"}</h6>
-                        <h6 className="card-title">Début : {event.beginDate.toLocaleDateString(undefined, {year: "numeric", month: "long", day: "numeric"})}</h6>
-                        <h6 className="card-title">Fin : {event.endDate.toLocaleDateString(undefined, {year: "numeric", month: "long", day: "numeric"})}</h6>
-                        <p className="card-text">{event.description}</p>
+						<h5 className="card-title">{event.name}</h5>
+						<p className="card-text">
+							{event.user && <span>Créé par <Link to={`/profile/${event.user.id}`} className="badge badge-primary" style={{ fontSize: '100%' }}>{event.user.username}</Link></span>} &middot;&nbsp;
+                        	{isSameDay ? 'Le' : 'Du'} {event.beginDate.toLocaleDateString()}
+							{!isSameDay && <span> au {event.endDate.toLocaleDateString()}</span>}
+						</p>
                         <Link to={`/events/${event.id}`} className="btn btn-primary">Détails</Link>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
