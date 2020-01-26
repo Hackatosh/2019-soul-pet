14164eafce4a46957f01966546a4662c5302e87c
benauwe <edouard.benauw@student.ecp.fr>
2020-01-26 11:39:27 +0100

    Added form for event

---
+++ front/src/components/EventForm.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import {Animal, PetEvent, Specie} from '../models';
+import { Modal, Form, Alert, Button } from 'react-bootstrap';
+import { Formik } from 'formik';
+import { AnimalService, AuthenticationService } from '../services';
+import {EventService} from "../services/event.service";
+
+export interface EventFormProps {
+    /** If in edition mode, the current values of the event */
+    event?: PetEvent,
+    /** Called to hide the modal */
+    onHide: () => void,
+    /** Whether or not to show the modal */
+    show: boolean,
+    /** Called when the post of the event was successful */
+    onSuccess: (event:PetEvent) => void
+}
+
+export interface EventFormState {
+    error: string;
+    species: Specie[];
+}
+
+/**
+ * A form used to add or edit an event
+ * TODO : ADD SPECIES SUPPORT
+ */
+export class EventForm extends React.Component<EventFormProps, EventFormState> {
+    constructor(props: EventFormProps) {
+        super(props);
+
+        this.state = { error: '', species: [] };
+    }
+
+    componentDidMount() {
+        AnimalService.getSpecies().then(species => this.setState({ species: species })).catch(() => this.setState({ error: 'Erreur lors de la récupération des espèces' }));
+    }
+
+    render() {
+        return (
+            <Modal onHide={this.props.onHide} show={this.props.show} aria-labelledby="contained-modal-title-vcenter" centered>
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">{this.props.event === undefined ? "Ajouter un évènement" : "Modifier un évènement" }</Modal.Title>
+                </Modal.Header>
+                <Formik onSubmit={values => {
+                    const event = this.props.event === undefined ? {} as PetEvent : this.props.event;
+                    event.name = values.name;
+                    event.beginDate = new Date(values.beginDate);
+                    event.endDate = new Date(values.endDate);
+                    event.description = values.description;
+
+                    // If we are adding a new event
+                    if (this.props.event === undefined)
+                        EventService.add(event).then(e => {
+                            this.props.onSuccess(e);
+                            this.props.onHide();
+                        }).catch(() => this.setState({ error: 'Erreur lors de la création de l’évènement' }));
+                    // Else we are editing an existing event
+                    else
+                        EventService.update(event).then(_ => {
+                            this.props.onSuccess(event);
+                            this.props.onHide();
+                        }).catch(() => this.setState({ error: 'Erreur lors de la mise à jour de l’évènement' }));
+                }}
+                        initialValues={this.props.event === undefined ?
+                            { name: '', beginDate: '', endDate: '', description:'', userId:AuthenticationService.User.id } :
+                            { name: this.props.event.name, beginDate: this.props.event.beginDate.toISOString().substr(0, 10), endDate: this.props.event.endDate.toISOString().substr(0, 10), description: this.props.event.description }}>
+                    {props => (
+                        <Form onSubmit={props.handleSubmit}>
+                            <Modal.Body>
+                                {this.state.error !== '' &&
+                                <Alert variant="danger">{this.state.error}</Alert>
+                                }
+                                <Form.Group controlId="eventName">
+                                    <Form.Label>Quel est le nom de votre évènement&nbsp;?</Form.Label>
+                                    <Form.Control name="name" type="text" placeholder="Entrez le nom de l'évènement" onChange={props.handleChange} value={props.values.name} required />
+                                </Form.Group>
+                                <Form.Group controlId="eventBeginDate">
+                                    <Form.Label>Quand commence-t-il ?&nbsp;?</Form.Label>
+                                    <Form.Control type="date" name="beginDate" onChange={props.handleChange} value={props.values.beginDate} required />
+                                </Form.Group>
+                                <Form.Group controlId="eventEndDate">
+                                    <Form.Label>Quand se termine-t-il&nbsp;?</Form.Label>
+                                    <Form.Control type="date" name="endDate" onChange={props.handleChange} value={props.values.endDate} required />
+                                </Form.Group>
+                                <Form.Group controlId="eventDescription">
+                                    <Form.Label>Une petite description&nbsp;?</Form.Label>
+                                    <Form.Control name="name" type="text" placeholder="Entrez la description de l'évènement" onChange={props.handleChange} value={props.values.description} required />
+                                </Form.Group>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="primary" type="submit">{this.props.event === undefined ? 'Ajouter' : 'Éditer'}</Button>
+                                <Button variant="secondary" onClick={this.props.onHide}>Annuler</Button>
+                            </Modal.Footer>
+                        </Form>
+                    )}
+                </Formik>
+            </Modal>
+        );
+    }
+}
--- front/src/services/event.service.ts
+++ front/src/services/event.service.ts
@@ -12,4 +12,31 @@ export class EventService {
     static async get(id: number): Promise<PetEvent> {
         return httpClient.get<PetEvent>(`/events/${id}`, true).then(this.revive).catch(() => Promise.reject('Erreur lors de la récupération de l\'event'));
     }
+
+    /**
+     * Adds an event.
+     * @param event the event to add
+     * @returns the event saved into the database
+     */
+    static async add(event:PetEvent): Promise<PetEvent> {
+        return httpClient.post<PetEvent>('/events/', event, true).then(EventService.revive).catch(() => Promise.reject(`Erreur lors de la création de l'évènement : ${event.name}`));
+    }
+
+    /**
+     * Updates an existing event.
+     * @param event the new event with updated values
+     * @returns the event saved into the database
+     */
+    static async update(event:PetEvent): Promise<PetEvent> {
+        return httpClient.put<PetEvent>(`/events/${event.id}`, event, true).then(EventService.revive).catch(() => Promise.reject(`Erreur lors de la mise à jour de l'évènement d'identifiant ${event.id}`));
+    }
+
+    /**
+     * Deletes an event from the database.
+     * @param id the id of the event to delete
+     * @returns null
+     */
+    static async delete(id: number): Promise<null> {
+        return httpClient.delete(`/events/${id}`, true).then(() => null).catch(() => Promise.reject(`Erreur lors de la suppression de l'évènement d'identifiant ${id}`));
+    }
 }
