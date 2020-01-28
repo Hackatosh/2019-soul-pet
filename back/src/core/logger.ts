dd69888b3e1cce75d2b68eac30a1e542f96cef8d
benauwe <edouard.benauw@student.ecp.fr>
2020-01-28 18:55:33 +0100

    Fixed some stuff

--- back/src/core/logger.ts
+++ back/src/core/logger.ts
@@ -5,46 +5,40 @@ import {env} from "../config/env";
 
 const winston = require('winston');
 
-const consoleConfig = [
-    new winston.transports.Console({
-        'colorize': true
-    })
-];
-
-const createLogger = new winston.Logger({
-    'transports': consoleConfig
-});
-
-const infoLogger = createLogger;
-infoLogger.add({
-    'name': 'info-file',
-    'level': 'info',
-    'filename': './logs/info.log',
-    'json': false,
-    'datePattern': 'yyyy-MM-dd-',
-    'prepend': true,
-});
-
-const errorLogger = createLogger;
-errorLogger.add({
-    'name': 'error-file',
-    'level': 'error',
-    'filename': './logs/error.log',
-    'json': false,
-    'datePattern': 'yyyy-MM-dd-',
-    'prepend': true,
+const winstonLogger = winston.createLogger({
+    transports: [
+        new (winston.transports.Console)({
+            colorize: true,
+            json:false,
+        }),
+        new (winston.transports.File)({
+            name: 'info-file',
+            level: 'info',
+            filename: './logs/info.log',
+            json: false,
+            datePattern: 'yyyy-MM-dd-',
+            prepend: true, }),
+        new (winston.transports.File)({
+            name: 'error-file',
+            level: 'error',
+            filename: './logs/error.log',
+            json: false,
+            datePattern: 'yyyy-MM-dd-',
+            prepend: true,
+        })
+    ]
 });
 
 const logInfo = env.LOG_IN_FILE ?
     function (info: string) {
-        infoLogger.info(info);
+        winstonLogger.info(info);
     }
     :
     console.log;
 
 const logError = env.LOG_IN_FILE ?
     function (err: string) {
-        errorLogger.error(err);
+        winstonLogger.error(err);
     }
     :
     console.log;
