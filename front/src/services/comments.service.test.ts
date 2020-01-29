a60ffb47f78dc46cb5d5b678edd80b3dd14b991f
Antoine Apollis <antoine@apollis.eu>
2020-01-29 19:39:13 +0100

    Increase test coverage

--- front/src/services/comments.service.test.ts
+++ front/src/services/comments.service.test.ts
@@ -16,10 +16,18 @@ const comment: EventComment = {
 test('Post comment', async () => {
 	post.mockResolvedValue(comment);
 	jest.spyOn(AuthenticationService, "User", "get").mockReturnValue({ id: 1, email: '' });
-	await CommentsService.post(comment).catch(e => expect(e).toBeDefined());
-	comment.text = 'B';
-	await CommentsService.post(comment).then(c => {
-		expect((post.mock.calls[0][1] as EventComment).userId).toBe(0);
+	let error: string;
+	await CommentsService.post(comment).catch(e => {
+		expect(e).toBeDefined();
+		error = e;	
+		comment.text = 'B';
+		CommentsService.post(comment).then(c => {
+			expect((post.mock.calls[0][1] as EventComment).userId).toBe(0);
+		});
+		post.mockRejectedValueOnce('');
+		CommentsService.post(comment).catch(e => {
+			expect(e).not.toBe(error);
+		});
 	});
-	expect.assertions(2);
+	expect.assertions(3);
 });
