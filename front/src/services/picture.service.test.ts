c457b117ed15a275a5d358dd3f7d49def796c243
Antoine Apollis <antoine@apollis.eu>
2020-01-31 18:34:52 +0100

    Increase test coverage

--- front/src/services/picture.service.test.ts
+++ front/src/services/picture.service.test.ts
@@ -1,13 +1,23 @@
 import { httpClient } from "../helpers";
-import { Picture } from "../models";
+import { Picture, Directory } from "../models";
 import { PictureService } from "./picture.service";
 
 const get = jest.spyOn(httpClient, "get");
 
 const picture: Picture = {id: 0, filename: 'image.jpg', content: ''};
 
-test('Get picture', async () => {
+test('Get picture error', async () => {
 	get.mockRejectedValue('');
-	await PictureService.loadPictureContent('repo', picture).catch(_ => expect(get.mock.calls[0][0]).toBe(`/pictures/repo/?filename=${picture.filename}`));
+	await PictureService.loadPictureContent(Directory.Animals, picture).catch(_ => expect(get.mock.calls[0][0]).toBe(`/pictures/${Directory.Animals}/?filename=${picture.filename}`));
+	expect.assertions(1);
+});
+
+test('Get picture', async () => {
+	const blob = new Blob();
+	get.mockResolvedValue(blob);
+	Object.defineProperty(window.URL, 'createObjectURL', { value: () => {} });
+	await PictureService.loadPictureContent(Directory.Animals, picture).then(p => {
+		expect(p.content).toBe(URL.createObjectURL(blob));
+	});
 	expect.assertions(1);
 });
