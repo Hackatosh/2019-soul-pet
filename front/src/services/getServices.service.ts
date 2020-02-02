525de7f6159340146b04930a7848b548de3bfcf7
Raklyon <alexis.hubert@student.ecp.fr>
2020-02-02 19:17:07 +0100

    Added doc as comments for services

--- front/src/services/getServices.service.ts
+++ front/src/services/getServices.service.ts
@@ -2,8 +2,20 @@ import { httpClient } from '../helpers';
 import { ListMarkerData, MarkerData } from '../models';
 
 
-
+/**
+ * This is used to retrieve the services given a location and a radius.
+ */
 export class GetServicesServices{
+    /**
+     * Retrieves a list of services for a specific type. It queries the `/places`
+     * back API.
+     * @param lat latitude of the user
+     * @param lon longitude of the user
+     * @param radius radius around which to find services
+     * @param placeType type of service to look for
+     * @returns listMarkerData list of markers with details such as position
+     * or address (string) about found services
+     */
     static async get(lat: number, lon: number, radius: number, placeType: string): Promise<ListMarkerData>{
       var listMarkerData: ListMarkerData = [];
       return httpClient.get<any>('/places/search?lat='+ lat +'&long='+ lon +'&radius='+ radius +'&placeType='+ placeType, true).then(venues => {
